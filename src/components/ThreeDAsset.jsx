8c168ea4ce5b0f4d5b8e93fa1b22df9904907099
Aishanipach <aishani.pachauri@gmail.com>
2025-02-12 12:22:56 +0530

    Added: Responsiveness for 3D asset

--- src/components/ThreeDAsset.jsx
+++ src/components/ThreeDAsset.jsx
@@ -1,14 +1,28 @@
+import { useWindowSize } from "@react-hook/window-size";
 import { OrbitControls, useGLTF } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
+import { useEffect, useState } from "react";
 
 const Model = () => {
   const { scene } = useGLTF("/assets/base_basic_pbr.glb"); // Load GLB file
   return <primitive object={scene} scale={3} />;
 };
 export default function ThreeDAsset() {
+  const [width] = useWindowSize();
+  const [cameraPosition, setCameraPosition] = useState([9, 7, 5]);
+
+  useEffect(() => {
+    console.log(width);
+    if (width > 500) {
+      setCameraPosition([9, 7, 5]);
+    } else {
+      setCameraPosition([21, 7, 5]);
+    }
+  }, [width]);
+
   return (
-    <div style={{ height: "80vh" }}>
-      <Canvas frameloop="demand" camera={{ position: [1, 7, 5] }}>
+    <div style={{ height: "80vh", marginTop: "15vh" }}>
+      <Canvas frameloop="always" camera={{ position: cameraPosition }}>
         <ambientLight intensity={0.8} />
         <directionalLight position={[3, 3, 3]} />
         <Model />
